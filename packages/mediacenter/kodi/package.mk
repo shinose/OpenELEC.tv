92c7a2859076356a0e0c03bee2c6b4cecf7b8074
Shinose <shinose@gmail.com>
2017-01-15 15:26:11 +0530

    bumped the kodi-qplay version to f0e956d

--- packages/mediacenter/kodi/package.mk
+++ packages/mediacenter/kodi/package.mk
@@ -37,7 +37,7 @@ case "$KODIPLAYER_DRIVER" in
     PKG_KEEP_CHECKOUT="no"
     ;;
   *)
-    PKG_VERSION="1d0c77d"
+    PKG_VERSION="f0e956d"
     PKG_GIT_URL="https://github.com/shinose/kodi-qplay.git"
     PKG_GIT_BRANCH="master"
     PKG_KEEP_CHECKOUT="yes"
